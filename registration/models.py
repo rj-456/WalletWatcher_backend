551f8cc5d7e32e03488b55a4d9f50e15592add4b
rj-456 <santosrancis04@gmail.com>
2025-12-03 07:17:55 +0800

    Rancis: Added date_registered field back

--- registration/models.py
+++ registration/models.py
@@ -5,7 +5,7 @@ class UserRegistration(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=250)
     password = models.CharField(max_length=150)
-
+    date_registered = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
