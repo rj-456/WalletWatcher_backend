6eb7db08e1c1684bb8fd246be4edbffd012b6274
rj-456 <santosrancis04@gmail.com>
2025-12-03 07:07:44 +0800

    Rancis: Added UserRegistration to Admin Panel

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserRegistration
 
-# Register your models here.
+# Define how the table looks in the Admin Panel
+class UserRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email') # Columns to show
+    search_fields = ('first_name', 'last_name', 'email')       # Enable search bar
+
+# Register the model with the customized view
+admin.site.register(UserRegistration, UserRegistrationAdmin)
